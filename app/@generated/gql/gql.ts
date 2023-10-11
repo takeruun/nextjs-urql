411efef2d20e5d1e4b8e237c158c831ea3023b8e
takeru.hashimoto <hstake25@gmail.com>
2023-10-11 22:38:46 +0900

    graphql コード生成

--- app/@generated/gql/gql.ts
+++ app/@generated/gql/gql.ts
@@ -13,7 +13,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-    "\n  query SearchItems($where: ItemWhere!) {\n    searchItems(where: $where) {\n      id\n      item {\n        title\n      }\n    }\n  }": types.SearchItemsDocument,
+    "\n  query SearchItems($where: ItemWhere!) {\n    searchItems(where: $where) {\n      id\n      item {\n        title\n        description\n      }\n    }\n  }": types.SearchItemsDocument,
 };
 
 /**
@@ -33,7 +33,7 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  query SearchItems($where: ItemWhere!) {\n    searchItems(where: $where) {\n      id\n      item {\n        title\n      }\n    }\n  }"): (typeof documents)["\n  query SearchItems($where: ItemWhere!) {\n    searchItems(where: $where) {\n      id\n      item {\n        title\n      }\n    }\n  }"];
+export function graphql(source: "\n  query SearchItems($where: ItemWhere!) {\n    searchItems(where: $where) {\n      id\n      item {\n        title\n        description\n      }\n    }\n  }"): (typeof documents)["\n  query SearchItems($where: ItemWhere!) {\n    searchItems(where: $where) {\n      id\n      item {\n        title\n        description\n      }\n    }\n  }"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
