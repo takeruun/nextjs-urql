4a7469a425b4e06313bf0767efa847ebf29cee1f
takeru.hashimoto <hstake25@gmail.com>
2023-10-12 02:46:45 +0900

    layout にもってくる

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,7 @@
-'use client';
-
-import { UrqlProvider, ssrExchange, cacheExchange, fetchExchange, createClient } from '@urql/next';
-import Items from './components/csr/Items';
-import { useMemo } from 'react';
-
 export default function Home() {
-  const [client, ssr] = useMemo(() => {
-    const ssr = ssrExchange();
-    const client = createClient({
-      url: 'http://localhost:8080/v1/graphql',
-      fetchOptions: {
-        headers:{
-          'x-hasura-admin-secret': 'secret'
-        }
-      },
-      exchanges: [cacheExchange, ssr, fetchExchange],
-      suspense: true,
-    });
-
-    return [client, ssr];
-  }, []);
-  
   return (
-    <UrqlProvider client={client} ssr={ssr}>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Items/>
-      </main>
-    </UrqlProvider>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      csr or ssr
+    </main>
   )
 }
