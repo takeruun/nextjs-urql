76c2dcab41023aaa802774a541ec8eee32ef0562
takeru.hashimoto <hstake25@gmail.com>
2023-10-20 19:56:00 +0900

    色々改善

--- app/csr/[id]/page.tsx
+++ app/csr/[id]/page.tsx
@@ -6,7 +6,7 @@ export default function Page({params}: {params: {id: string}}) {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="flex flex-col items-center justify-center w-full">
-        <ItemEditor id={params.id} />
+        <ItemEditor id={params.id} isPage={true} />
       </div>
     </main>
   )
